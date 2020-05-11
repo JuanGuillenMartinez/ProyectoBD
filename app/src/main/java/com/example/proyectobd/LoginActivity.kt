286e4b46b9cde0fc4ca20a6b54abd420a56ac069
Juan Guillen <juanguillenmtz16@gmail.com>
2020-05-10 20:28:53 -0500

    Login XML terminado

---
+++ app/src/main/java/com/example/proyectobd/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.proyectobd
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onClick(v: View?) {
+        if(btnIngresar.isClickable) {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun activar(v: View?) {
+        if(btnRegistrar.isClickable) {
+            Toast.makeText(this, "Prueba de ejecucion de los listener", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+}
